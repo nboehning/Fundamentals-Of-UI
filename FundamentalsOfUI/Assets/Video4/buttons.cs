bceeec5c4a71a3d889ee264acf4c5f5213fde37a
nboehning <nboehning@gmail.com>
2016-04-26 23:58:56 -0500

    Completed CGCookie build
    Completed CG Cookie

---
+++ FundamentalsOfUI/Assets/Video4/buttons.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class buttons : MonoBehaviour
+{
+
+    public Text chatText;
+
+    void Awake()
+    {
+        chatText = chatText.GetComponent<Text>();
+    }
+
+    public void PlayButton (int scene)
+    {
+		SceneManager.LoadScene(scene);
+	}
+
+    public void ExitButton ()
+	{
+		Application.Quit ();
+	}
+
+    public void ToggleButton(bool mainMenu)
+    {
+        if (mainMenu)
+            SceneManager.LoadScene("Video5Scene");
+    }
+
+    public void ChatInput(InputField chat)
+    {
+        chatText.text = chat.text;
+    }
+}
